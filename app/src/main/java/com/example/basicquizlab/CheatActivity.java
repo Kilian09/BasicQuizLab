1976b975990c92346c70127ea0123647a9f9f6fa
kilian <kilian.garcia106@alu.ulpgc.es>
2020-02-25 12:25:22 +0000

    Inicializados los valores del Layout de cheatActivity

--- app/src/main/java/com/example/basicquizlab/CheatActivity.java
+++ app/src/main/java/com/example/basicquizlab/CheatActivity.java
@@ -4,12 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+
+
+    private Button noButton, yesButton;
+    private TextView sureText, cheatReplyText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        initComponents();
+
+        initLayoutContent();
+    }
+
+    private void initComponents() {
+        noButton = findViewById(R.id.noButton);
+        yesButton = findViewById(R.id.yesButton);
+
+        sureText = findViewById(R.id.sureTextView);
+        cheatReplyText = findViewById(R.id.cheatReplyTextView);
+    }
+
+    private void initLayoutContent() {
+
+        noButton.setText(R.string.no_button_text);
+        yesButton.setText(R.string.yes_button_text);
+
+        sureText.setText(R.string.sure_text);
+        cheatReplyText.setText(R.string.empty_text);
     }
 }
